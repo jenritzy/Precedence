5198136ad49e6bf1280a298ade4bd41359e05632
jenritzy <Jenritzy@outlook.com>
2022-09-20 12:59:10 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Precedence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Precedence";
+            int sum;
+            sum = 1 + 4 * 3;
+            Console.Write("Default Order:\t\t" + sum);
+            sum = (1 + 4) * 3;
+            Console.Write("\nForced Order: \t\t" + sum);
+            sum = 7 - 4 + 2;
+            Console.Write("\n\nDefault Direction:\t" + sum);
+            sum = 7 - (4 + 2);
+            Console.Write("\nForced Direction: \t" + sum);
+            Console.ReadKey();
+        }
+    }
+}
